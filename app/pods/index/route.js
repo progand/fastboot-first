5450b2262bc44160dc82dfb89a9c231307c2891b
progand <lach@ukr.net>
2017-08-24 10:32:05 +0300

    [*] Fastboot simpla app

---
+++ app/pods/index/route.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import fetch from 'ember-fetch/ajax';
+
+export default Ember.Route.extend({
+    model() {
+        return fetch('https://api.github.com/users/progand/repos');
+      }
+});
